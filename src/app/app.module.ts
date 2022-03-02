7685e6efcaa7fda9a5ef0e57ff2efc93f681f81d
Pratapsingh <iamprataphajari@gmail.com>
2022-03-02 12:23:06 +0530

    AdminLte Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { RightSideBarComponent } from './common/right-side-bar/right-side-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    RightSideBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
