f2f6b1d2e08be1f4f0167adc3b3b78a257f0045b
Pratapsingh <iamprataphajari@gmail.com>
2022-03-07 13:48:50 +0530

    User DB Updated

--- server/todo.sql
+++ server/todo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2022 at 12:15 PM
+-- Generation Time: Mar 07, 2022 at 09:17 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.5.35
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `list` (
   `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `task` varchar(255) NOT NULL,
   `is_done` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0 = not_done, 1=done'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -36,10 +37,33 @@ CREATE TABLE `list` (
 -- Dumping data for table `list`
 --
 
-INSERT INTO `list` (`id`, `task`, `is_done`) VALUES
-(3, 'this is first task', 0),
-(4, 'imp', 0),
-(5, 'DO xyz', 0);
+INSERT INTO `list` (`id`, `user_id`, `task`, `is_done`) VALUES
+(3, 1, 'this is first task', 0),
+(4, 1, 'imp', 0),
+(5, 2, 'DO xyz', 0),
+(6, 1, 'this is new task', 0),
+(7, 2, 'this is akash new task', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `full_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `full_name`) VALUES
+(1, 'demo', 'demo', 'Pratap singh'),
+(2, 'akash', '123123', 'Akash Adhe');
 
 --
 -- Indexes for dumped tables
@@ -52,6 +76,13 @@ ALTER TABLE `list`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +91,11 @@ ALTER TABLE `list`
 --
 ALTER TABLE `list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
